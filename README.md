8c099786abf461b94dca1a076b6def7013be8b5f
Chinedu Ekene Okpala <allstackdev@gmail.com>
2024-08-18 02:59:10 +0000

    update

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # CoalitionTest
 Refreshing my Angular skills with this simple dashboard UI
 
-https://xd.adobe.com/view/121254c9-532f-4772-a1ba-dfe529a96b39-4741/
-
 I went advance to add a mock login, and created other pages.
 
 ## Development server
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'coalition-test' title`, () => {
+  it(`should have the 'angular-app-fresher' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('coalition-test');
+    expect(app.title).toEqual('angular-app-fresher');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, coalition-test');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-app-fresher');
   });
 });
